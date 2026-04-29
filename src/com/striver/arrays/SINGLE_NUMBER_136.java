f0bbb336e8d6732669c70c187463c9257df5456e
Yogeshch06 <chyogesh10b09@gmail.com>
2026-04-29 07:31:06 +0530

    Add this single no prg

--- src/com/striver/arrays/SINGLE_NUMBER_136.java
+++ src/com/striver/arrays/SINGLE_NUMBER_136.java
@@ -1,4 +1,17 @@
 package com.striver.arrays;
 
 public class SINGLE_NUMBER_136 {
+
+        public int singleNumber(int[] nums) {
+            int xorr=0;
+            for( int i = 0 ; i < nums.length ; i++){
+                xorr ^= nums[i];
+            }
+            return xorr;
+        }
+
+    static void main() {
+        SINGLE_NUMBER_136 obj = new SINGLE_NUMBER_136();
+        System.out.println(obj.singleNumber(new int[]{1,1,3,4,4,5,5}));
+    }
 }
