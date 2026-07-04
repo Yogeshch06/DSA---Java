8df6abfcbe4b7f93f825a462a3d72dd68cee4e99
Yogeshch06 <chyogesh10b09@gmail.com>
2026-07-04 23:49:16 +0530

    Add Leetcode_121 Brute force

---
+++ src/com/striver/arrays/DP_ON_STOCKS_121.java
@@ -0,0 +1,24 @@
+package com.striver.arrays;
+
+public class DP_ON_STOCKS_121 {
+    int maxProfit(int[] prices) {
+
+        //Brute force
+        int max = 0;
+        int cost = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                cost = prices[j] - prices[i];
+                if (max < cost)
+                    max = cost;
+                cost = 0;
+            }
+        }
+        return max;
+    }
+
+    static void main() {
+        DP_ON_STOCKS_121 s = new DP_ON_STOCKS_121();
+        System.out.println(s.maxProfit(new int[]{10, 9, 8, 1, 5, 4, 9, 10,11,9,4,8,15}));
+    }
+}
