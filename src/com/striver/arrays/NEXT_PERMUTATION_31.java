67cba59ad2edfee9275e702d158db40e6d3eaecd
Yogeshch06 <chyogesh10b09@gmail.com>
2026-07-08 00:35:55 +0530

    Add Leetcode_31

---
+++ src/com/striver/arrays/NEXT_PERMUTATION_31.java
@@ -0,0 +1,46 @@
+package com.striver.arrays;
+
+import java.util.Arrays;
+
+public class NEXT_PERMUTATION_31 {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        reverse(nums, index + 1, n - 1);
+    }
+    void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void main() {
+        NEXT_PERMUTATION_31 obj = new NEXT_PERMUTATION_31();
+        obj.nextPermutation(new int[] {1,1,5});
+    }
+}
